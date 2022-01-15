1abdfd746cfeaf7d05c1f34ab6fc7b4a4a5f8327
Paulo Marinho <pakas_vieira_95@hotmail.com>
2022-01-15 11:46:16 -0300

    feat: initial commit

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+// Just run the comand below to transbile without any file config:
+// ./node_modules/.bin/babel src --out-dir lib --presets=@babel/preset-react
+
+// Or, run "npm run build" to transpile with a differ framework function
+
+const App = (props) => {
+    <div>{"Hello!"}</div>
+};
